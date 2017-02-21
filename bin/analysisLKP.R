076d75d93ab956cf5a8035e4c9ca95e7b3ed3fe4
Lindsay Pino <lpino@uw.edu>
2017-02-21 09:59:51 -0800

    cleaned data with consolidated timecourse

--- bin/analysisLKP.R
+++ bin/analysisLKP.R
@@ -18,3 +18,19 @@ apply(all.df, 2, function(x){sum(is.na(x))})
 complete <- all.df %>% complete.cases()
 mean(complete)  ## proportion of cases complete
 length(unique(all.df$patientid))  ## total unique patients
+
+## subset for metadata features
+meta.df <- all.df %>% select(patientid, treatment, age, race, gender, duration_months)  # subset for features we'll be using 
+apply(meta.df, 2, function(x){sum(is.na(x))}) 
+complete <- meta.df %>% complete.cases()
+
+
+## Summary statistics
+ages <- all.df %>% 
+    group_by(as.factor(gender)) %>% 
+    summarise(mean(duration_months))
+
+hist(all.df$age)
+hist(all.df$duration_months)
+
+
